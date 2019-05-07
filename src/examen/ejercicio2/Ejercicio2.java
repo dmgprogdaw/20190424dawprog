6d9b7da253204cb7709b27a29264c9d6d762278e
David <zukoclase96@gmail.com>
2019-05-07 09:57:04 +0200

    Correccion recuperacion del 24 de abril

--- src/examen/ejercicio2/Ejercicio2.java
+++ src/examen/ejercicio2/Ejercicio2.java
@@ -1,32 +1,62 @@
 package examen.ejercicio2;
 
 import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Ejercicio2 {
-	static int n;
-	static int m;
-	static int numero;
+	
+	static Scanner teclado = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		Scanner teclado = new Scanner(System.in);
-		System.out.print("Introduce n y m: ");
-		n = teclado.nextInt();
-		m = teclado.nextInt();
-		
-		for(int i=0; i<n+m; i++) {
-			System.out.println("Introduce un numero:");
-			numero = teclado.nextInt();
+		Scanner linea;
+		System.out.print("> ");
+		try {
+			linea = new Scanner(teclado.nextLine());
+			int n = linea.nextInt();
+			int m = linea.nextInt();
+			if (linea.hasNext()) {
+				System.out.println("No se esperaban más números");
+				System.exit(-1);
+			}
+			Set<Integer> primerConjunto = leerConjunto(n);
+			Set<Integer> segundoConjunto = leerConjunto(m);
+			segundoConjunto.removeAll(primerConjunto);
+			System.out.println(segundoConjunto);
+		}catch(InputMismatchException e) {
+			System.out.println("Se esperaba un número entero");
+			System.exit(-1);
 		}
-		
-		System.out.print(leerConjunto());
+		catch(NoSuchElementException e) {
+			System.out.println("Se esperaban dos números en esta linea");
+			System.exit(-1);
+		}
+
+
 	}
 	
-	public static Set<Integer> leerConjunto () {
-		Set<Integer> primerConjunto = new HashSet<Integer>();
-		Set<Integer> segundoConjunto = new HashSet<Integer>();	
-		primerConjunto.add(numero);
-		segundoConjunto.add(numero);
-		return segundoConjunto;
+	public static Set<Integer> leerConjunto (int n) {
+		Set<Integer> s = new HashSet<>();
+		try {
+			for(int i=0; i<n; i++) {
+				Scanner linea = new Scanner(teclado.nextLine());
+				int num = linea.nextInt();
+				if (linea.hasNext()) {
+					System.out.println("No se esperaban más números");
+					System.exit(-1);
+				}
+				s.add(num);
+			}
+		}catch(InputMismatchException e) {
+			System.out.println("Se esperaba un número entero");
+			System.exit(-1);
+		}
+		catch(NoSuchElementException e) {
+			System.out.println("Se esperaban dos números en esta linea");
+			System.exit(-1);
+		}
+		return s;
 	}
 }
