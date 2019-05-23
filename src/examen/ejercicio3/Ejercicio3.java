bdd6509c67d8ec7cf95a02f179cdedf001b5a4e2
David <zukoclase96@gmail.com>
2019-05-23 14:16:31 +0200

    Ejer 3

--- src/examen/ejercicio3/Ejercicio3.java
+++ src/examen/ejercicio3/Ejercicio3.java
@@ -1,47 +1,154 @@
 package examen.ejercicio3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Ejercicio3 {
-	static Map<String, Map<String, Integer>> mensajes = new HashMap<>();
-	
-	public static void main(String[] args){
-		Scanner teclado = new Scanner(System.in);
-		int totalMensajes = 0;
-		int mensajeIp;
-		int numIp = 0;
-		String ip;
-		String mensaje;
-		String usuario;
-			
+
+	public static void main(String[] args) throws IOException{
+		Map<String, Map<String, Integer>> usuariosIp = new HashMap<>();
+		Map<String, Integer> mensajeIp = new HashMap<>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int totalMensajes = 0; //contador para el total de los mensajes
+		int numIp = 0; //contador para el total de ips
+		String ip = null , mensaje = null, usuario = null, linea;
+		boolean fin = false;	
 		do {
-			System.out.print("IP= ");
-			ip = teclado.nextLine();
-			System.out.print("mensaje= ");
-			mensaje = teclado.nextLine();
-			System.out.print("usuario= ");
-			usuario = teclado.nextLine();
-			
-			mensajeIp = 0;
-			mensajes.put(usuario, mensajes.put(ip, mensajes.get(mensajeIp)));
-				
-			if (mensajes.containsKey(usuario)) {
-				if (mensajes.containsKey(ip)) {			
-					mensajeIp = mensajeIp + 1;
+			System.out.print("> ");
+			Scanner teclado = new Scanner(br.readLine());
+			int estado = 0;
+			String token = null;
+			while(estado != 8) {
+				switch(estado) {
+					case 0:
+						try {
+							token = teclado.skip("terminar|Ip").match().group();
+							if (token.equals("terminar")) {
+								estado = 8;
+								fin = true;
+							}
+							else if (token.equals("Ip")) {
+								estado = 1;
+							}
+						} catch (NoSuchElementException e) {
+							System.out.println("Se esperaba terminar o Ip");
+							estado = 8;
+						}
+						break;
+					case 1:
+						try {				
+							if(token == "Ip") {
+								teclado.skip("=");	
+								estado = 2;
+							}
+							else if (token == "mensaje") {
+								teclado.skip("=");	
+								estado = 4;
+							}
+							else if (token == "usuario") {
+								teclado.skip("=");	
+								estado = 6;
+							}
+						} catch (NoSuchElementException e) {
+							System.out.println("Se esperaba el simbolo '='");
+							estado = 8;
+						}
+						break;
+					case 2:
+						try {
+							ip = teclado.skip("((\\d{1,2}|1\\d{2}|2[0-4]\\d|25[0-5]).){3}(?:\\d{1,2}|1\\d{2}|2[0-4]\\d|25[0-5])").match().group();
+							estado = 3;
+							//(?:(?:\\d{1,2}|1\\d{2}|2[0-4]\\d|25[0-5]).){3}(?:\\d{1,2}|1\\d{2}|2[0-4]\\d|25[0-5])A
+						} catch (NoSuchElementException e) {
+								System.out.println("Se esperaba una dirección ip válida");
+							estado = 8;
+						}
+						break;
+					case 3:
+						try {
+							token = teclado.skip("\\s*mensaje").match().group();					
+							estado = 1;
+						} catch (NoSuchElementException e) {
+							System.out.println("Se esperaba 'mensaje'");
+							estado = 8;
+						}
+						break;
+					case 4:
+						try {
+							mensaje = teclado.skip("\\p{L}+").match().group();					
+							estado = 5;
+						} catch (NoSuchElementException e) {
+							System.out.println("Se esperaba un mensaje");
+							estado = 8;
+						}
+						break;
+					case 5:
+						try {
+							token = teclado.skip("\\s*usuario").match().group();					
+							estado = 1;
+						} catch (NoSuchElementException e) {
+							System.out.println("Se esperaba 'usuario'");
+							estado = 8;
+						}
+						break;
+					case 6:
+						try {
+							usuario = teclado.skip("\\p{L}+").match().group();
+						} catch (NoSuchElementException e) {
+							System.out.println("Se esperaba un usuario");
+							estado = 8;
+						}
+						break;
 				}
-				numIp = numIp +1;
-				totalMensajes = totalMensajes + mensajeIp;
 			}
-			
-			System.out.println("nombre de usuario: " + usuario);
-			System.out.println(ip + "=> " + mensajeIp);
-			System.out.println("Número de Ips: " + numIp);
-			System.out.println("Total de mensajes: " + totalMensajes);
-				
-		}while(!ip.equals("terminar"));
+		}while (!fin);
 	}
 }
 
-
+/*
+ case 3:
+						try {
+							token = teclado.skip("mensaje").match().group();
+							estado = 1;
+						} catch (NoSuchElementException e) {
+								System.out.println("Se esperaba mensaje");
+							estado = 8;
+						}
+					case 4:
+						try {
+							mensaje = teclado.skip("\\p{L}+").match().group();
+							estado = 5;
+						} catch (NoSuchElementException e) {
+								System.out.println("Se esperaba un mensaje");
+							estado = 8;
+						}
+					case 5:
+						try {
+							mensaje = teclado.skip("usuario").match().group();
+							estado = 1;
+						} catch (NoSuchElementException e) {
+								System.out.println("Se esperaba usuario");
+							estado = 8;
+						}
+					case 6:
+						try {
+							usuario = teclado.skip("\\p{L}+").match().group();
+							
+						} catch (NoSuchElementException e) {
+								System.out.println("Se esperaba un usuario");
+							estado = 8;
+						}
+					case 7:
+						try {
+							usuario = teclado.skip("\\p{L}+").match().group();
+							
+						} catch (NoSuchElementException e) {
+								System.out.println("Se esperaba un usuario");
+							estado = 8;
+						}
+*/
